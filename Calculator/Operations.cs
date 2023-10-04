be89b826138050ced41ce492cbca1d2b3f658d41
DESKTOP-UMQJN5C\Ferna <fernandoacurcio@hotmail.com>
2023-10-04 10:45:46 +0100

    [dev] - created a calculator machine

---
+++ Calculator/Operations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Operations
+    {
+        /// <summary>
+        /// Operation method, this can return null
+        /// null here works as a default value
+        /// </summary>
+        /// <param name="numbA"></param>
+        /// <param name="numbB"></param>
+        /// <param name="operationType"></param>
+        /// <returns></returns>
+        public static float? Operation(float numbA, float numbB, int operationType)
+        {
+            switch (operationType)
+            {
+                case 1:
+                    return numbA + numbB;
+                case 2:
+                    return numbA - numbB;
+                case 3:
+                    return numbA * numbB;
+                case 4:
+                    if (numbB == 0)
+                        return null; // it's impossible to divide by zero so returns null
+                    return numbA / numbB;
+                default:
+                    return null;
+            }
+        }
+
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculator;
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Calculator;
 
 /* This project is a improvement from the previous project Math Game
 *  the intention with this project is to build a simple calculator
@@ -8,7 +10,73 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        CalculatorHandler();
+    }
 
-        Console.WriteLine("Hello, World!");
+    private static void CalculatorHandler()
+    {
+        do
+        {
+            StartMenu();
+            var auxOperation = int.TryParse(Console.ReadLine(), out int op);
+
+            if (op == 0)
+                break;
+
+            if (op < 0 || op > 4)
+            {
+                Console.WriteLine("Choose a valid Option");
+            }    
+            else
+            {
+                var values = ValidateNumbers();
+                if (values.Item1 == 0 && op == 4)
+                {
+                    Console.WriteLine("It's impossible to divide by Zero!!!");
+                }
+                else
+                {
+                    Console.WriteLine($"Result: {Operations.Operation(values.Item1, values.Item2, op)}");
+                }
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+        }
+        while (true);
+    }
+
+    private static (float, float) ValidateNumbers()
+    {
+        float numbA, numbB;
+
+        while (true)
+        {
+            Console.Write("Insert a valid number A: ");
+            if (float.TryParse(Console.ReadLine(), out numbA))
+                break; // Exit the loop if a valid number is entered.
+        }
+
+        while (true)
+        {
+            Console.Write("Insert a valid number B: ");
+            if (float.TryParse(Console.ReadLine(), out numbB))
+                break; // Exit the loop if a valid number is entered.
+        }
+
+        return (numbA, numbB);
+    }
+
+    private static void StartMenu()
+    {
+        Console.WriteLine("--- Welcome to the calculator ---");
+        Console.WriteLine("Choose from the following options");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("1- Addition;");
+        Console.WriteLine("2- Subtraction;");
+        Console.WriteLine("3- Multiplication;");
+        Console.WriteLine("4- Division;");
+        //Console.WriteLine("5- Game History;");
+        Console.WriteLine("Click 0 to exit");
     }
 }
