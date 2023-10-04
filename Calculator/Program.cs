00fb319eec441c8298a91a18be6e72b6eb6f3a2a
DESKTOP-UMQJN5C\Ferna <fernandoacurcio@hotmail.com>
2023-10-04 02:30:27 +0100

    [dev] -  added calculator project

---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator;
+
+/* This project is a improvement from the previous project Math Game
+*  the intention with this project is to build a simple calculator
+*  this calculater can do operations with decimal numbers.
+*/
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, World!");
+    }
+}
