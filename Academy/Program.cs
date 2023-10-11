b8f328e2e62d2e91f9b0d2fff717092107d296b7
DESKTOP-UMQJN5C\Ferna <fernandoacurcio@hotmail.com>
2023-10-11 17:11:45 +0100

    [dev] - insert method

--- Academy/Program.cs
+++ Academy/Program.cs
@@ -3,12 +3,57 @@
 // Creating migrations: dotnet ef migrations add InitialMigration
 
 using Academy;
+using Academy.Model;
 using Microsoft.EntityFrameworkCore;
 
 var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite().Options;
 var dbContext = new ApplicationDbContext(options);
 
 // create the database
-dbContext.Database.Migrate();
+//dbContext.Database.Migrate();
 
+ProcessInsert();
 
+void ProcessInsert()
+{
+    var address = new Address()
+    {
+        City = "Braga",
+        Street = "Rua nº2",
+        Zip = "4700",
+        HouseNumber = 2
+    };
+    var professor = new Professor()
+    {
+        FirstName = "John",
+        LastName = "Smith",
+        Address = address,
+    };
+    var student1 = new Student()
+    {
+        FirstName = "Peter",
+        LastName = "Parker",
+        Address = address,
+    };
+    var student2 = new Student()
+    {
+        FirstName = "Maria",
+        LastName = "Carey",
+        Address = address,
+    };
+    var class1 = new Class()
+    {
+        Professor = professor,
+        Students = new List<Student> { student1, student2 },
+        Title = "IT Class"
+    };
+
+    dbContext.Addresses.Add(address);
+    dbContext.Professors.Add(professor);
+    dbContext.Students.Add(student1);   
+    dbContext.Students.Add(student2);
+    dbContext.Classes.Add(class1);
+
+    dbContext.SaveChanges();
+    dbContext.Dispose();
+}
