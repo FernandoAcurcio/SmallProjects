22d8d12109b8b89983b1e9d4d1343dc27f7116a6
DESKTOP-UMQJN5C\Ferna <fernandoacurcio@hotmail.com>
2023-10-11 16:54:10 +0100

    [dev] - create and migrate database

---
+++ Academy/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Academy.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Academy;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Professor> Professors { get; set; }
+    public DbSet<Class> Classes { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Filename=Academy.db");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // define primary key
+        modelBuilder.Entity<Address>().HasKey(x => x.Id);
+        modelBuilder.Entity<Student>().HasKey(x => x.Id);
+        modelBuilder.Entity<Professor>().HasKey(x => x.Id);
+        modelBuilder.Entity<Class>().HasKey(x => x.Id);
+
+        // define relations
+        modelBuilder.Entity<Student>().HasOne(x => x.Address);
+        modelBuilder.Entity<Professor>().HasOne(x => x.Address);
+
+        // one class can have many students and one student can have many classes
+        modelBuilder.Entity<Class>().HasMany(x => x.Students).WithMany(x => x.Classes);
+        modelBuilder.Entity<Class>().HasOne(x => x.Professor).WithMany(x => x.Classes);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- Academy/Program.cs
+++ Academy/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Academy
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// What to do to install and migrate databases
+// Installin EF Core tools: tool install --global dotnet-ef
+// Creating migrations: dotnet ef migrations add InitialMigration
+
+using Academy;
+using Microsoft.EntityFrameworkCore;
+
+var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite().Options;
+var dbContext = new ApplicationDbContext(options);
+
+// create the database
+dbContext.Database.Migrate();
+
+
