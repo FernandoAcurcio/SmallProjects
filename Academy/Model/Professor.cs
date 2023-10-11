f6d4cdfad67b77189768107d553755bf7ad57ad4
DESKTOP-UMQJN5C\Ferna <fernandoacurcio@hotmail.com>
2023-10-11 11:03:57 +0100

    [dev]- created base model for project

---
+++ Academy/Model/Professor.cs
@@ -0,0 +1,9 @@
+﻿namespace Academy.Model;
+
+public class Professor : BaseEntity
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public List<Class> Classes { get; set; } = default!;
+    public Address Address { get; set; } = default!;
+}
---
+++ Academy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
