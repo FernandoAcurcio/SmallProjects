5849b283162f3384f44e1ddf1887e12b869e86a1
DESKTOP-UMQJN5C\Ferna <fernandoacurcio@hotmail.com>
2023-10-12 15:12:51 +0100

    [dev] - created database

---
+++ EmployeeManagement.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeManagement.Common.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=EmployeeManagement.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Address>().HasKey(x => x.Id);
+            builder.Entity<Employee>().HasKey(x => x.Id);
+            builder.Entity<Team>().HasKey(x => x.Id);
+            builder.Entity<Job>().HasKey(x => x.Id);
+
+            builder.Entity<Employee>().HasOne(x => x.Address);
+            builder.Entity<Employee>().HasOne(x => x.Job);
+
+            builder.Entity<Team>().HasMany(x => x.Employees).WithMany(x => x.Teams);
+
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
